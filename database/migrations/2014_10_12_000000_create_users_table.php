005cb38a63f6f833b0046eca8d60862165bd9f54
Md. Asaduzzaman <md.asad.mony@gmail.com>
2020-11-16 01:37:44 +0600

    migrations done

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -19,6 +19,11 @@ class CreateUsersTable extends Migration
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            $table->string('userType')->default('user');
+            $table->boolean('isActivated')->default(0);
+            $table->string('passwordResetCode')->nullable();
+            $table->string('activationCode')->nullable();
+            $table->string('socialType')->nullable();
             $table->rememberToken();
             $table->timestamps();
         });
